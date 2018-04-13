cf58695f6d6786d65af517de8dbd1cc82efd5479
kasldfh <forumf41l@gmail.com>
2018-04-13 14:03:53 -0500

    updated scripts

--- make_client_tuns.sh
+++ make_client_tuns.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo ip link delete tun3
 openvpn --mktun --dev tun3
 ip link set tun3 up
 ip addr add 192.168.0.1/24 dev tun3
--- make_server_tuns.sh
+++ make_server_tuns.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+#clear existing tunnel
+ip link delete tun4
+
 openvpn --mktun --dev tun4
 ip link set tun4 up
 ip addr add 192.168.0.2/24 dev tun4
 
-echo "created interface tun3 with ip 192.168.0.2"
+
+echo "created interface tun4 with ip 192.168.0.2"
